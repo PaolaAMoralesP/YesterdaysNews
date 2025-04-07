fc70e5d85ffc470b86650a50b8e103b6af7db97c
kat-lev <kat@katleverton.com>
2025-04-07 15:55:45 +0200

    Create repository and model

---
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/model/Article.java
@@ -0,0 +1,8 @@
+package com.yesterdaysnews.yesterdaysnews.model;
+
+public class Article {
+    private int id;
+    private String title;
+    private String content;
+    private String publicationDate;
+}

b9df4e79a57b5331c71ec21c761e3ca3ae3c5b1e
Paola Morales <dissenyaspasia@gmail.com>
2025-04-07 11:32:38 +0200

    Start project

---
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/YesterdaysnewsApplication.java
@@ -0,0 +1,13 @@
+package com.yesterdaysnews.yesterdaysnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesterdaysnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesterdaysnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yesterdaysnews/yesterdaysnews/YesterdaysnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesterdaysnews.yesterdaysnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesterdaysnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

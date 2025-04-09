32fe8e00409ec0d6cbfaddaed0291a05a216598b
einartech <imalovelyghost13@gmail.com>
2025-04-09 16:00:06 +0200

    feature-delete-user: working properly

--- src/main/java/com/yesterdaysnews/yesterdaysnews/controller/UserController.java
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.yesterdaysnews.yesterdaysnews.controller;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +19,17 @@ import jakarta.validation.Valid;
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService){
-    this.userService = userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUserById(@PathVariable Integer id) {
+        return userService.deleteUserById(id);
     }
 }
--- src/main/java/com/yesterdaysnews/yesterdaysnews/service/UserService.java
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/service/UserService.java
@@ -5,8 +5,7 @@ import com.yesterdaysnews.yesterdaysnews.model.User;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;  
-
+import org.springframework.stereotype.Service;
 
 @Service
 
@@ -17,12 +16,18 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
-    // Add methods to interact with the book repository
+
     public ResponseEntity<Object> createUser(User user) {
         userRepository.save(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
-    
-}
-}
+    }
 
+    public ResponseEntity<Object> deleteUserById(Integer id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+    }
+}

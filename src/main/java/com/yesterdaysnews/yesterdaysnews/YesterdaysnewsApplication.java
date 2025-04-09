ad3be36c2ebaed2bdf2813bcee80b931af33c655
einartech <imalovelyghost13@gmail.com>
2025-04-09 09:57:00 +0200

    feat-postgresql working fine with .env file

--- src/main/java/com/yesterdaysnews/yesterdaysnews/YesterdaysnewsApplication.java
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/YesterdaysnewsApplication.java
@@ -2,11 +2,19 @@ package com.yesterdaysnews.yesterdaysnews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class YesterdaysnewsApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
+
 		SpringApplication.run(YesterdaysnewsApplication.class, args);
 		System.out.println("Yesterdaysnews application started successfully!");
 	}

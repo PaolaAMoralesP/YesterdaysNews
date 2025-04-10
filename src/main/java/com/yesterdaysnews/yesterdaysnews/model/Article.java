1a7492c14513efe474d25be279946d63c1980e4c
Paola Morales <dissenyaspasia@gmail.com>
2025-04-10 15:20:11 +0200

    Add validation to Article Entity

--- src/main/java/com/yesterdaysnews/yesterdaysnews/model/Article.java
+++ src/main/java/com/yesterdaysnews/yesterdaysnews/model/Article.java
@@ -1,18 +1,32 @@
 package com.yesterdaysnews.yesterdaysnews.model;
 
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "articles")
 // Clase que representa un artículo
+
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    //Add validation
+
+
+    @NotBlank(message = "Title is required")
+    @Size(max=255, message = "Title must have max 255 characters")
     private String title;
+
+    @NotBlank(message = "Content is required")
+    @Size(min=50, max=2000, message = "Content must have min 50 and max 2000 characters")
     private String content;
+
+    @Column(name = "publicationDate")
     private String publicationDate;
+
+
     // usuario aparece aki
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
